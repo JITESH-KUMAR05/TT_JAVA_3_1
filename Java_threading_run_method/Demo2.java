519429e9e6487ac1bd15ea8a26338640047645be
Jitesh Kumar <150531216+JITESH-KUMAR05@users.noreply.github.com>
2025-09-15 10:09:33 +0000

    synchronized method

--- Java_threading_run_method/Demo2.java
+++ Java_threading_run_method/Demo2.java
@@ -3,9 +3,9 @@ class A extends Thread
 {
     int total=0;
     public void run(){
-        for(int i=0;i<10000;i++){
-            this.total = this.total + i;
-        }
+        // for(int i=0;i<10000;i++){
+        //     this.total = this.total + i;
+        // }
         /*
          * for example if we have 1 cr statements after this 
          * for loop then the main thread will wait for 
@@ -13,6 +13,12 @@ class A extends Thread
          * the total value 
          * so now we will use wait, notify, notify all;
          */
+        synchronized(this){
+            for(int i=0;i<10000;i++){
+                this.total = this.total + i;
+            }
+            this.notify();
+        }
     }
 }
 
@@ -25,7 +31,11 @@ class B{
         // a2.start();
         // Thread.sleep(1000); // we are waiting unnecessary time so we will use join method
         // a1.join(); // this is also enfficient so we will use wait,notify,notify all
-        
+        synchronized(a1){
+            a1.wait();
+
+        }
+
         System.out.println(a1.total);
     }
 }
